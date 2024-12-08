1cd4420b7ad0ef6db6e035ff47061b2cf17811d3
Dishank Patil <dishankjp05@gmail.com>
2024-12-08 13:40:05 +0530

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+  cout << std::unitbuf;
+  cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  //
-  // std::string input;
-  // std::getline(std::cin, input);
+  cout << "$ ";
+  
+  string input;
+  getline(cin, input);
+
+return 0;
 }
