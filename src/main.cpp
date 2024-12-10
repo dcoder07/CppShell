3c0d2ef6f02e4eb8f95cb03e6acac0e5a6093c0d
Dishank Patil <dishankjp05@gmail.com>
2024-12-10 23:13:41 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,25 @@ int main()
   cout << unitbuf;
   cerr << unitbuf;
 
-  for (;;)
+  string input;
+  cout << "$ ";
+  getline(cin, input);
+  if (input == "exit 0")
+    return 0;
+
+  if (input.substr(0, 5) == "echo ")
+    cout << input.substr(5) << endl;
+
+  if (input.substr(0, 5) == "type ")
   {
-    string input;
-    cout << "$ ";
-    getline(cin, input);
-    if (input == "exit 0")
-      return 0;
-    if (input.substr(0, 4) == "echo")
-      cout << input.substr(5) << endl;
-    if (input.substr(0, 5) == "type ")
-    {
-      if (input.substr(5) == "exit" || input.substr(5) == "echo" || input.substr(5) == "type")
-        cout << input.substr(5) << " is a shell builtin" << endl;
-      else
-        cout << input.substr(5) << ": not found" << endl;
-    }
+    if (input.substr(5) == "exit" || input.substr(5) == "echo" || input.substr(5) == "type")
+      cout << input.substr(5) << " is a shell builtin" << endl;
     else
-      cout << input << " : command not found" << endl;
+      cout << input.substr(5) << ": not found" << endl;
   }
+
+  else
+    cout << input << " : command not found" << endl;
 
   return 0;
 }
