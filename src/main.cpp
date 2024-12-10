63978717a24f1c0cc74d3668ef89a8eaf797dcce
Dishank Patil <dishankjp05@gmail.com>
2024-12-10 23:01:18 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ int main()
     getline(cin, input);
     if (input == "exit 0")
       return 0;
-
+    if (input.substr(0, 4) == "echo")
+      cout << input.substr(5) << endl;
     if (input.substr(0, 5) == "type ")
     {
       if (input.substr(5) == "exit" || input.substr(5) == "echo" || input.substr(5) == "type")
@@ -22,7 +23,7 @@ int main()
         cout << input.substr(5) << ": not found" << endl;
     }
     else
-      cout << input << ": command not found" << endl;
+      cout << input << " : command not found" << endl;
   }
 
   return 0;
