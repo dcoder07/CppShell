3bad184c865a84189a98a465b4ddd6274d6f1090
Dishank Patil <dishankjp05@gmail.com>
2024-12-12 00:26:56 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -3,50 +3,39 @@ using namespace std;
 
 int main()
 {
-  bool exit = 0;
+  int exit = 0;
   while (!exit)
   {
-    /* code */
-
-    // Flush after every std::cout / std:cerr
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // std::cout << "Logs from your program will appear here!\n";
-    // Uncomment this block to pass the first stage
-    std::cout << "$ ";
-    bool checkValid = 0;
-    std::string input;
-    std::getline(std::cin, input);
+    cout << std::unitbuf;
+    cerr << std::unitbuf;
+    int checkValid = 0;
+    string input;
+    cout << "$ ";
+    getline(cin, input);
 
     if (input == "exit 0")
     {
-      checkValid = 1;
       exit = 1;
+      checkValid = 1;
     }
+
     if (input.substr(0, 5) == "type ")
     {
       checkValid = 1;
-      std::string cmd = input.substr(5);
-      if (cmd.substr(0, 4) == "type" || cmd.substr(0, 4) == "exit" || cmd.substr(0, 4) == "echo")
-      {
-        std::cout << cmd << " is a shell builtin\n";
-      }
+      if (input.substr(5, 4) == "type" || input.substr(5, 4) == "echo" || input.substr(5, 4) == "exit")
+        cout << input << " is a shell builtin" << endl;
       else
-      {
-        std::cout << cmd << " not found\n";
-      }
+        cout << input << " not found" << endl;
     }
+
     if (input.substr(0, 5) == "echo ")
     {
       checkValid = 1;
-      std::string toPrint = input.substr(5);
-      std::cout << toPrint << '\n';
+      cout << input.substr(5) << endl;
     }
+
     if (checkValid == 0)
-    {
-      std::cout << input << ": command not found\n";
-    }
+      cout << input << ": command not found" << endl;
   }
   return 0;
 }
