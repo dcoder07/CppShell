5fbcaaa89f2d15b970d933649d462846e092bec1
Dishank Patil <dishankjp05@gmail.com>
2024-12-12 00:38:00 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,46 @@ using namespace std;
 
 int main()
 {
-  int exit = 0;
+  bool exit = 0;
   while (!exit)
   {
-    cout << std::unitbuf;
-    cerr << std::unitbuf;
-    int checkValid = 0;
-    string input;
     cout << "$ ";
+    bool checkValid = 0;
+    string input;
     getline(cin, input);
 
     if (input == "exit 0")
     {
-      exit = 1;
       checkValid = 1;
+      exit = 1;
     }
 
     if (input.substr(0, 5) == "type ")
     {
       checkValid = 1;
-      string s = input.substr(0, 5);
-      if (s.substr(0, 4) == "type" || s.substr(0, 4) == "echo" || s.substr(0, 4) == "exit")
-        cout << input << " is a shell builtin" << endl;
+      string cmd = input.substr(5);
+      if (cmd.substr(0, 4) == "type" || cmd.substr(0, 4) == "exit" || cmd.substr(0, 4) == "echo")
+      {
+        cout << cmd << " is a shell builtin\n";
+      }
       else
-        cout << input << " not found" << endl;
+      {
+        cout << cmd << " not found\n";
+      }
     }
 
     if (input.substr(0, 5) == "echo ")
     {
       checkValid = 1;
-      cout << input.substr(5) << endl;
+      string toPrint = input.substr(5);
+      cout << toPrint << '\n';
     }
 
-    if (checkValid == 0)
-      cout << input << ": command not found" << endl;
+    if (!checkValid)
+    {
+      cout << input << ": command not found\n";
+    }
   }
+
   return 0;
 }
