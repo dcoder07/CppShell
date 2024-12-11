a1c45fab40ef61b3f80c1a792dba9605a403c2aa
Dishank Patil <dishankjp05@gmail.com>
2024-12-12 00:12:55 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,36 @@ using namespace std;
 
 int main()
 {
-  for (;;)
+  int exit = 0, checkValid = 0;
+  while (!exit)
   {
     cout << unitbuf;
     cerr << unitbuf;
     string input;
+    checkValid = 0;
     cout << "$ ";
     getline(cin, input);
 
     if (input == "exit 0")
-      return 0;
+    {
+      exit = 1;
+      checkValid = 1;
+    }
 
     if (input.substr(0, 5) == "type ")
     {
-      if (input.substr(5, 4) == "exit" || input.substr(5, 4) == "echo" || input.substr(5, 4) == "type")
-        cout << input.substr(5) << " is a shell builtin" << endl;
+      checkValid = 1;
+      if (input.substr(5, 4) == "echo" || input.substr(5, 4) == "exit" || input.substr(5, 4) == "type")
+        cout << input << " is a shell builtin" << endl;
       else
-        cout << input.substr(5) << ": not found" << endl;
+        cout << input << " not found" << endl;
     }
 
     if (input.substr(0, 5) == "echo ")
       cout << input.substr(5) << endl;
 
-    else
-      cout << input << " : command not found" << endl;
+    if (checkValid == 0)
+      cout << input << ": command not found" << endl;
   }
 
   return 0;
