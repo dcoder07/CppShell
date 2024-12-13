f5bcb03ca66b3a617aac59f1d2158d08636f104c
Dishank Patil <dishankjp05@gmail.com>
2024-12-14 00:35:30 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,73 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <filesystem>
+#include <cstdlib> // For getenv
+
 using namespace std;
-bool isValid(string command)
-{
-  string str = command.substr(0, command.find(" "));
-  if (str == "type" || str == "echo" || str == "exit" || str == "cd")
-    return true;
-  return false;
+
+// Function to check if the command is a valid shell builtin
+bool isValid(string command) {
+    string str = command.substr(0, command.find(" "));
+    return (str == "type" || str == "echo" || str == "exit" || str == "cd");
 }
 
-string getPathEnv(string command)
-{
-  string path_env = getenv("PATH");
-  stringstream ss(path_env);
-  string temp;
-  while (!ss.eof())
-  {
-    getline(ss, temp, ':');
-    string commandPath = temp + '/' + command;
-    if (filesystem::exists(commandPath))
-      return commandPath;
-  }
-  return "";
-}
+// Function to get the absolute path of a command from the PATH environment variable
+string getPathEnv(string command) {
+    string path_env = getenv("PATH");
+    stringstream ss(path_env);
+    string temp;
 
-int main()
-{
-  bool exit = false;
-  while (!exit)
-  {
-    cout << unitbuf;
-    cerr << unitbuf;
-    cout << "$ ";
-    string input;
-    getline(cin, input);
-
-    string cmd = input.substr(5);
-
-    switch (isValid(input))
-    {
-    case cd:
-      break;
-    case echo:
-      cout << cmd << endl;
-    case exit0:
-      exit = true;
-      break;
-    case type:
-      if (isValid(cmd))
-        cout << cmd << " is a shell builtin" << endl;
-      else
-      {
-        string path = getPathEnv(cmd);
-        if (!path.empty())
-          cout << input << " is " << path << endl;
-        else
-          cout << input << " not found" << endl;
-      }
-    default:
-      cout << input << ": command not found" << endl;
+    while (getline(ss, temp, ':')) { // Tokenize PATH variable
+        string commandPath = temp + '/' + command;
+        if (filesystem::exists(commandPath)) {
+            return commandPath;
+        }
     }
-  }
-  return 0;
+    return "";
+}
+
+int main() {
+    bool exitShell = false;
+
+    while (!exitShell) {
+        cout << "$ "; // Shell prompt
+        string input;
+        getline(cin, input);
+
+        if (input.empty()) {
+            continue; // Ignore empty input
+        }
+
+        string command = input.substr(0, input.find(" ")); // Extract command name
+        string args = input.substr(input.find(" ") + 1);   // Extract arguments
+
+        if (command == "cd") {
+            cout << "cd: command not implemented yet" << endl;
+        } else if (command == "echo") {
+            cout << args << endl;
+        } else if (command == "exit") {
+            exitShell = true; // Exit the shell
+        } else if (command == "type") {
+            if (isValid(args)) {
+                cout << args << " is a shell builtin" << endl;
+            } else {
+                string path = getPathEnv(args);
+                if (!path.empty()) {
+                    cout << args << " is " << path << endl;
+                } else {
+                    cout << args << ": command not found" << endl;
+                }
+            }
+        } else {
+            string path = getPathEnv(command);
+            if (!path.empty()) {
+                cout << command << " found at: " << path << endl;
+            } else {
+                cout << command << ": command not found" << endl;
+            }
+        }
+    }
+
+    return 0;
 }
