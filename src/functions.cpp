dacff8f5b3901594bef6d41c5da26566784dfd3d
Dishank Patil <dishankjp05@gmail.com>
2024-12-26 16:02:07 +0530

    [any message]

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,19 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-enum commandType
-{
-    builtIn,
-    executable,
-    ext_executable,
-    nonexistent
-};
-
-struct fullCommandType
-{
-    commandType type;
-    string execPath;
-};
 
 string findCommandExecPath(string command)
 {
