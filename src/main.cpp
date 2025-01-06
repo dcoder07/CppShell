026ebd9d6399a198a8da3275701ef32620996691
Dishank Patil <dishankjp05@gmail.com>
2025-01-06 20:10:32 +0530

    [Testing Logic]

--- src/main.cpp
+++ src/main.cpp
@@ -155,13 +155,11 @@ void evaluateCatCommand(char ch, string &input)
   vector<string> paths_vec;
   while (i < input.size())
   {
-    if (input[i] == ch || input[i] == '/')
+    if (input[i] == ch)
     {
-      if (input[i] != '/')
-        i++;
-        
+      i++;
       string path = "";
-      while (input[i] != ch && i < input.size())
+      while (input[i] != ch)
         path += input[i++];
       paths_vec.push_back(path);
     }
